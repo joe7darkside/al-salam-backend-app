f20fe7da515a904e30dfebf105eb7b191fdda382
joe7darkside <jo2014saleh@gmail.com>
2022-05-25 00:13:29 +0300

    user UI

--- resources/views/dashboard/users/overview.blade.php
+++ resources/views/dashboard/users/overview.blade.php
@@ -157,8 +157,8 @@
                 </div>
             </div>
 
-            @include('dashboard.components.footer')
-            @yield('footer')
+            {{-- @include('dashboard.components.footer')
+            @yield('footer') --}}
         </div>
     </main>
 
